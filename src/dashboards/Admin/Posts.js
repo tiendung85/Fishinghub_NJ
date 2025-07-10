03ddb3e911e0ad2d2c409795dd6cfe6ef3947656
tiendung <dungg5171@gmail.com>
2025-07-10 18:26:19 +0700

    Add files via upload

---
+++ src/dashboards/Admin/Posts.js
@@ -0,0 +1,8 @@
+export default function Posts() {
+  return (
+    <div className="main-content">
+      <h1>New Feed Page</h1>
+      {/* Add more content or components as needed */}
+    </div>
+  );
+}
